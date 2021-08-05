70277ac1bdf0d437a5fcd001d6bb69ebe583be17
David-raj123 <64373160+David-raj123@users.noreply.github.com>
2021-08-06 02:50:38 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -4,15 +4,15 @@ import os
 
 
 class Config(object):
-    API_ID = int(os.environ.get("API_ID", 12345))
+    API_ID = int(os.environ.get("API_ID", 1543212))
     API_HASH = os.environ.get("API_HASH", "")
     BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-    SESSION_NAME = os.environ.get("SESSION_NAME", "Rename-Bot-0")
+    SESSION_NAME = os.environ.get("SESSION_NAME", "TheRename_Robot")
     SLEEP_TIME = int(os.environ.get("SLEEP_TIME", 5))
-    BOT_OWNER = os.environ.get("BOT_OWNER", 1445283714)
-    CAPTION = "Rename Bot by @{}\n\nMade by @AbirHasan2005"
+    BOT_OWNER = os.environ.get("BOT_OWNER", 1016768333)
+    CAPTION = "**© @TheRename_Robot**"
     UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", None)
-    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", -100))
+    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", -1001261444869))
     MONGODB_URI = os.environ.get("MONGODB_URI", "")
     DOWNLOAD_PATH = os.environ.get("DOWNLOAD_PATH", "./downloads")
     BROADCAST_AS_COPY = bool(os.environ.get("BROADCAST_AS_COPY", False))
