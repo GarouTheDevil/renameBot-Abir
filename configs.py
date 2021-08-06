cc56dbd198f9e45ff7e6fbb762d4842bf03f6515
David-raj123 <64373160+David-raj123@users.noreply.github.com>
2021-08-06 12:36:26 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -4,19 +4,19 @@ import os
 
 
 class Config(object):
-    API_ID = int(os.environ.get("API_ID", ""))
+    API_ID = int(os.environ.get("API_ID", "")
     API_HASH = os.environ.get("API_HASH", "")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", ""))
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
     SESSION_NAME = os.environ.get("SESSION_NAME", "TheRename_Robot"))
-    SLEEP_TIME = int(os.environ.get("SLEEP_TIME", ""))
-    BOT_OWNER = os.environ.get("BOT_OWNER", ""))
+    SLEEP_TIME = int(os.environ.get("SLEEP_TIME", "")
+    BOT_OWNER = os.environ.get("BOT_OWNER", "")
     CAPTION = "**© @TheRename_Robot**"
     UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "David_Botz"))
     LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", ""))
-    MONGODB_URI = os.environ.get("MONGODB_URI", ""))
-    DOWNLOAD_PATH = os.environ.get("DOWNLOAD_PATH", "./downloads"))
-    BROADCAST_AS_COPY = bool(os.environ.get("BROADCAST_AS_COPY", False))
-    ONE_PROCESS_ONLY = bool(os.environ.get("ONE_PROCESS_ONLY", False))
+    MONGODB_URI = os.environ.get("MONGODB_URI", "")
+    DOWNLOAD_PATH = os.environ.get("DOWNLOAD_PATH", "./downloads")
+    BROADCAST_AS_COPY = bool(os.environ.get("BROADCAST_AS_COPY", False)
+    ONE_PROCESS_ONLY = bool(os.environ.get("ONE_PROCESS_ONLY", False)
     START_TEXT = """ I Am Rename Bot
     """
     PROGRESS = """
